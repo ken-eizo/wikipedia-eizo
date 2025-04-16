a8a138958fbe68f0bdb701257e23f545b5da6882
ken-eizo <toriken1013@gmail.com>
2025-04-17 00:11:50 +0900

    refactor: firebase config to inline

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -6,30 +6,21 @@ import { GoogleAuthProvider } from "firebase/auth";
 import { getAuth } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 
-let app;
-let auth;
-let provider;
-let db;
-let storage;
+const firebaseConfig = {
+    apiKey: "AIzaSyA3H5GJGVKtzYKBRqPlnRpeZ6yIC4HNiRg",
+    authDomain: "wikipediaeizo.firebaseapp.com",
+    projectId: "wikipediaeizo",
+    storageBucket: "wikipediaeizo.firebasestorage.app",
+    messagingSenderId: "282678401680",
+    appId: "1:282678401680:web:263ec3787153a7c3c01615",
+    measurementId: "G-M03TBS744E"
+};
 
-// クライアントサイドでのみ初期化
-if (typeof window !== 'undefined') {
-    const firebaseConfig = {
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-    };
-
-    app = initializeApp(firebaseConfig);
-    auth = getAuth(app);
-    provider = new GoogleAuthProvider();
-    db = getFirestore(app);
-    storage = getStorage(app);
-}
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const storage = getStorage(app);
 
 export { auth, provider, storage };
 export default db;
