2a1592cc3338c9d0a213cf31acf379adfa1f7d9f
ken-eizo <toriken1013@gmail.com>
2025-04-17 23:21:19 +0900

    sidebar 更新

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -12,6 +12,7 @@ const Sidebar = () => {
         <div className="sidebar-section">
           <Link href="/readme">readme</Link>
           <Link href="/newrelease">今後追加予定</Link>
+          <Link href="https://eizo.mov/">eizo.mov</Link>
           <Link href="https://team.eizo.mov/">team.eizo.mov</Link>
         </div>
       </div>
